e7e2014b99d7122ef06b216a7d384ef9fa05b362
Abhinav Lakhara <abhinavlakhara8@gmail.com>
2025-07-12 11:59:39 +0530

    Basic

---
+++ Basic.java
@@ -0,0 +1,12 @@
+public class Basic {
+
+    public static void main(String[] args){
+        //Single Line Comment
+        /*Multi
+        Line
+        Comment
+        */
+        System.out.println("Hello this is my journey on java");
+        System.out.println("I love java");
+    }
+}
